adc4b1641dc827fe5ab6f6ac1e9a198380a4088f
Hala M. Dalloul <haladalloul42@gmail.com>
2026-03-10 12:20:44 +0200

    Implement project details view and task management
    - Add `ProjectDetailsFragment` to display project info and its associated tasks from Firestore.
    - Implement `TaskAdapter` and `item_task.xml` layout to display task lists with status-based color coding.
    - Create `Task` and `ProjectMember` domain models.
    - Add `task_menu.xml` for project actions (Update, Delete, Delete All Tasks).
    - Implement a project update dialog in `ProjectDetailsFragment` to modify project name and description.
    - Add navigation logic in `ProjectsFragment` to open `ProjectDetailsFragment` on item click.
    - Define Firestore collection constants in a new `Collections` utility class.
    - Add required string resources and drawables for tasks and project editing.

---
+++ app/src/main/java/com/example/cleardayapplication/domain/utils/Collections.java
@@ -0,0 +1,13 @@
+package com.example.cleardayapplication.domain.utils;
+
+public class Collections {
+
+    public static final String PROJECTS = "Project";
+    public static final String TASKS = "Task";
+    public static final String USERS = "User";
+    public static final String PROJECT_ID="projectId";
+    public static final String PROJECT_NAME = "name";
+    public static final String PROJECT_DESCRIPTION = "description";
+
+
+}
--- app/src/main/java/com/example/cleardayapplication/ui/fragments/project/ProjectsFragment.java
+++ app/src/main/java/com/example/cleardayapplication/ui/fragments/project/ProjectsFragment.java
@@ -21,17 +21,12 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProjectsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ProjectsFragment extends Fragment implements OnItemClicks {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private String mParam1;
+    private String mParam2;
 
     private FragmentProjectsBinding binding;
     private ProjectAdapter adapter;
@@ -39,23 +34,11 @@ public class ProjectsFragment extends Fragment implements OnItemClicks {
     private FirebaseFirestore firestore;
     private FirebaseAuth auth;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public ProjectsFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProjectsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static ProjectsFragment newInstance(String param1, String param2) {
         ProjectsFragment fragment = new ProjectsFragment();
         Bundle args = new Bundle();
@@ -128,6 +111,9 @@ public class ProjectsFragment extends Fragment implements OnItemClicks {
 
     @Override
     public void onCardItemClick(Object obj) {
-
+        getActivity().getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container,ProjectDetailsFragment.newInstance(obj))
+                .commit();
     }
 }
